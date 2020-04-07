87a9eea4c7cb7fbad80e19f9ed2aebf36329c788
darlene <darlene.costa@unitelco.com.br>
2020-04-07 11:45:08 -0300

    implentando servidor utilizando apollo-server, graphQl e do lado do cliente o axios

--- client/src/components/DomainsList.vue
+++ client/src/components/DomainsList.vue
@@ -32,6 +32,7 @@
 </template>
 
 <script>
+import axios from 'axios/dist/axios';
 import AppItemList from "./AppItemList";
 export default {
 	name: 'DomainsList',
@@ -40,8 +41,8 @@ export default {
 	},
 	data: function () {
 		return {		
-			prefixes: ['Air', 'Jet', 'Flight'],
-			sufixes: ['Hub', 'Station', 'Mart']
+			prefixes: [],
+			sufixes: []
 		};
 	}, 
 	methods:{
@@ -71,6 +72,31 @@ export default {
 			}
 			return domains;
 		}
+	},
+	created() {
+		axios ({
+			url: 'http://localhost:4000',
+			method: "post",
+			data: {
+				query:`
+				{
+					prefixes: items (type: "prefix") {
+							id
+							type
+							description
+					}
+					sufixes: items (type: "sufix") {
+						id
+						type
+						description
+					}
+				}`
+			}
+		}).then(response =>{
+			const query = response.data;
+			this.prefixes = query.data.prefixes.map(prefix => prefix.description);
+			this.sufixes = query.data.sufixes.map(sufix => sufix.description);		
+		});
 	}
 };
 </script>
